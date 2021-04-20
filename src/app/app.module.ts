59336dcdedb12ccb6d693e4a6200d4b6039e797d
Guadalupe Díaz <201820250@tese.edu.mx>
2021-04-20 13:24:15 -0500

    Creacion del modulo Contador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { HeroesModule } from './heroes/heroes.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroeComponent,
-    ListadoComponent,
   ],
 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {}
